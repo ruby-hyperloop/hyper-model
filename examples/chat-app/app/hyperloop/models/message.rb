9636aeef292ed521987d3b90cf6ced7ca02dc3e6
Jan Biedermann <jan@kursator.de>
2018-01-07 09:58:54 +0100

    move example app to examples repo

--- examples/chat-app/app/hyperloop/models/message.rb
+++
@@ -1,6 +0,0 @@
-require 'models/application_record'
-class Message < ApplicationRecord
-  def time
-    created_at
-  end
-end
--- examples/chat-app/db/migrate/20170319194429_create_message.rb
+++
@@ -1,9 +0,0 @@
-class CreateMessage < ActiveRecord::Migration[5.0]
-  def change
-    create_table :messages do |t|
-      t.string :from
-      t.text :message
-      t.timestamps
-    end
-  end
-end
